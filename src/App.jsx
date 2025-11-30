097d9a3d9c95a1b681ec81b06fb969b45f4c6630
Caio Carvalho <k4io12345@gmail.com>
2025-11-30 00:09:20 -0300

    :start vite project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Caio Carvalho</h1>
+      <h2>Digital Solutions</h2>
+      <a href="https://caiocarvalho.dev.br" target='_blank'>www.caiocarvalho.dev.br</a>
+    </>
+  )
+}
+
+export default App

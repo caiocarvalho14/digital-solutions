909638e0a54098b8e9eba4e25cf519021e1bc272
Caio Carvalho <k4io12345@gmail.com>
2025-12-05 12:10:19 -0300

    update links contato

--- src/components/components/contato/contato.jsx
+++ src/components/components/contato/contato.jsx
@@ -6,8 +6,8 @@ export default function Contato() {
                 <h1 className='mb-4 fw-bold'>Pronto para <strong className='text-primary'>Transformar</strong> Seu Negócio?</h1>
                 <h1 className='text-secondary fs-4'>Vamos conversar sobre como posso ajudar a alcançar seus objetivos com tecnologia de ponta</h1>
                 <div className='d-flex flex-column justify-content-center gap-4 flex-sm-row align-items-center py-3'>
-                    <a href="" className='btn btn-outline-dark'><i className='bi bi-envelope mx-2'></i> Enviar Email <i className='bi bi-arrow-right'></i></a>
-                    <a href="" className='btn btn-primary'><i className='bi bi-chat-left mx-2'></i> WhatsApp</a>
+                    <a href="mailto:caioscphb@gmail.com" className='btn btn-outline-dark'><i className='bi bi-envelope mx-2'></i> Enviar Email <i className='bi bi-arrow-right'></i></a>
+                    <a href="https://wa.me/86995633313" className='btn btn-primary'><i className='bi bi-chat-left mx-2'></i> WhatsApp</a>
                 </div>
                 <hr />
                 <small className=' text-secondary'>Resposta em até 24h • Consulta gratuita • Sem compromisso</small>
